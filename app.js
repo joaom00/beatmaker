8571cf8a12b85cd33e6328f097d2af3d064e9896
joaom <joaomljcanal@gmail.com>
2021-02-16 21:41:34 -0300

    Adding Tempo

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit {
     this.isPlaying = null;
     this.selects = document.querySelectorAll('select');
     this.muteBtns = document.querySelectorAll('.mute');
+    this.tempoSlider = document.querySelector('.tempo-slider');
   }
 
   repeat() {
@@ -116,6 +117,21 @@ class DrumKit {
       }
     }
   }
+
+  changeTempo(e) {
+    const tempoText = document.querySelector('.tempo-nr');
+    tempoText.innerText = e.target.value;
+  }
+
+  updateTempo(e) {
+    this.bpm = e.target.value;
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    const playBtn = document.querySelector('.play');
+    if (playBtn.classList.contains('active')) {
+      this.start();
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -142,4 +158,12 @@ drumKit.muteBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     drumKit.mute(e);
   });
+});
+
+drumKit.tempoSlider.addEventListener('input', (e) => {
+  drumKit.changeTempo(e);
+});
+
+drumKit.tempoSlider.addEventListener('change', (e) => {
+  drumKit.updateTempo(e);
 });
