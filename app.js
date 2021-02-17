e9ad63db2616dc72c473184d5c6aee920e2bd6c0
joaom <joaomljcanal@gmail.com>
2021-02-16 21:10:31 -0300

    Muting Sounds

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
     this.bpm = 150;
     this.isPlaying = null;
     this.selects = document.querySelectorAll('select');
+    this.muteBtns = document.querySelectorAll('.mute');
   }
 
   repeat() {
@@ -85,6 +86,36 @@ class DrumKit {
         break;
     }
   }
+
+  mute(e) {
+    const muteIndex = e.target.getAttribute('data-track');
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 0;
+          break;
+        case '1':
+          this.snareAudio.volume = 0;
+          break;
+        case '2':
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 1;
+          break;
+        case '1':
+          this.snareAudio.volume = 1;
+          break;
+        case '2':
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -104,5 +135,11 @@ drumKit.playBtn.addEventListener('click', () => {
 drumKit.selects.forEach((select) => {
   select.addEventListener('change', (e) => {
     drumKit.changeSound(e);
+  });
+});
+
+drumKit.muteBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    drumKit.mute(e);
   });
 });
